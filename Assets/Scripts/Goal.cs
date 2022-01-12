6f21b3b10eb69acb0f5904319dc7d8191588ea13
AtmacaSevval <sevvallatmaca@gmail.com>
2022-01-12 22:02:22 +0300

    arranged

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        BallControler ball = other.GetComponent<BallControler>();
+
+        if(!ball || GameManager.singleton.GameEnded)
+        {
+            return;
+        }
+
+        Debug.Log("Goal was touched");
+        GameManager.singleton.EndGame(true);
+    }
+}
